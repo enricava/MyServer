f20db00557e564aa79cf770a601f72b1bb72fa58
Enrique Cavanillas <enriquecavanillaspuga@gmail.com>
2022-06-27 16:34:10 +0200

    Client can send messages to Server. Updated Readme to include copy pasters.

--- client.cpp
+++ client.cpp
@@ -2,7 +2,7 @@
  * @file client.cpp
  * @author Enrique Cavanillas Puga
  * @brief This is a dummy-test client that allows user to send text using TCP protocol.
- * @version 0.1
+ * @version 0.2
  * @date 2022-06-27
  * 
  */
@@ -19,30 +19,32 @@
 
 int main(int argc, char *argv[]){
     if (argc < 3){
-        fprintf(stderr, "Usage: %s host port msg...\n", argv[0]);
+        fprintf(stderr, "Usage: %s serv %s port msg...\n", argv[0], argv[1]);
         exit(EXIT_FAILURE);
     }
 
-    struct addrinfo hints;
-    struct addrinfo *result, *rp;
-    int sfd, s, j;
-    size_t len;
-    ssize_t nread;
+    struct addrinfo hints, *result, *rp;
+    sockaddr_storage addr;
+    socklen_t addrlen = sizeof(addr);
+    int sfd;
     char buf[BUF_SIZE];
 
     /* Obtain address(es) matching host/port */
 
     memset(&hints, 0, sizeof(struct addrinfo));
     hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
-    hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
+    hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
     hints.ai_flags = 0;
     hints.ai_protocol = 0; /* Any protocol */
+    hints.ai_canonname = NULL;
+    hints.ai_addr = NULL;
+    hints.ai_next = NULL;
 
-    s = getaddrinfo(argv[1], argv[2], &hints, &result);
-    if (s != 0){
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+    if (getaddrinfo(argv[1], argv[2], &hints, &result)){
+        fprintf(stderr, "Error: getaddrinfo %s : %s\n", argv[1], argv[2]);
         exit(EXIT_FAILURE);
     }
+
     /*getaddrinfo() returns a list of address structures.
     Try each address until we successfully connect(2).
     If socket(2)(or connect(2)) fails, we(close the socket and) try the next address.*/
@@ -59,43 +61,25 @@ int main(int argc, char *argv[]){
         close(sfd);
     }
 
-    if (rp == NULL)
-    { /* No address succeeded */
+    if (rp == NULL){ /* No address succeeded */
         fprintf(stderr, "Could not connect\n");
         exit(EXIT_FAILURE);
     }
 
     freeaddrinfo(result); /* No longer needed */
 
-    /* Send remaining command-line arguments as separate
-       datagrams, and read responses from server */
-
-    for (j = 3; j < argc; j++)
-    {
-        len = strlen(argv[j]) + 1;
-        /* +1 for terminating null byte */
-
-        if (len > BUF_SIZE)
-        {
-            fprintf(stderr,
-                    "Ignoring long message in argument %d\n", j);
-            continue;
+    while (true) {
+        int c = read(0, buf, BUF_SIZE);     /* Read client input */
+        buf[c] = '\0';
+        if (buf[0] == 'q' && c == 2){
+            close(sfd);
+            printf("Closing connection\n");
+            exit(EXIT_SUCCESS);
         }
-
-        if (write(sfd, argv[j], len) != len)
-        {
-            fprintf(stderr, "partial/failed write\n");
-            exit(EXIT_FAILURE);
-        }
-
-        nread = read(sfd, buf, BUF_SIZE);
-        if (nread == -1)
-        {
-            perror("read");
-            exit(EXIT_FAILURE);
-        }
-
-        printf("Received %zd bytes: %s\n", nread, buf);
+        send(sfd, buf, c, 0);               /* Send client input */
+        c = recv(sfd, buf, BUF_SIZE, 0);    /* Read server message */
+        buf[c] = '\0';
+        printf("\tServer: %s",buf);
     }
 
     exit(EXIT_SUCCESS);
