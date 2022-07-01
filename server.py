dc29ad614bfa3e7ab9bf3469809a87a67318a394
Enrique Cavanillas <enriquecavanillaspuga@gmail.com>
2022-07-01 13:07:04 +0200

    Multiprocess enabled

--- server.py
+++ server.py
@@ -1,39 +1,80 @@
 """
 Echo server
-v 0.1
+v 0.2
 """
+from http import client
 import socket
 import os
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def server_init(num_connections=5):
+    """
+    Creates TCP socket, binds and listens.
+    Starts dispatcher.
 
-# Bind the socket to the port
-server_address = ('localhost', 2222)
-print('[+] Server starting up on {} port {}'.format(*server_address))
-sock.bind(server_address)
+    param1: number of simultaneous connectons (int)
+    """
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Listen for incoming connections
-sock.listen(1)
+    # Bind the socket to the port
+    server_address = ('localhost', 2222)
+    print('[+] Server starting up on {} port {}'.format(*server_address))
+    sock.bind(server_address)
 
-while True:
-    # Wait for a connection
-    print('[+] Waiting for connections')
-    connection, client_address = sock.accept()
+    # Listen for incoming connections
+    sock.listen(num_connections)
+
+    active_children = []
+
+    run_dispatcher(sock, active_children)
+
+
+
+def handle_connection(connection, client_address):
+    """
+    Handles new connection.
+    """
+    pid = os.getpid()
     try:
-        print('[+] Connection from', client_address)
-
+        print( pid, '[+] Connection from',client_address)
         # Receive the data in small chunks and retransmit it
         while True:
             data = connection.recv(16)
-            print('[+] Received {!r}'.format(data))
+            print( pid, '[+] Received {!r}'.format(data))
             if data:
-                print('[+] Sending data back to the client')
+                print( pid, '[+] Sending data back to the client')
                 connection.sendall(data)
             else:
-                print('[+] Received no data', client_address)
+                print( pid, '[+] Received no data', client_address)
                 break
 
     finally:
         # Clean up the connection
         connection.close()
+        os._exit(0)
+
+
+def run_dispatcher(sock, active_children):
+    """
+    Handles multi-process connections
+
+    param 1: tcp socket (socket)
+    param 2: list of active children pids (list[int])
+    """
+    while True:
+        # Wait for a connection, accept and fork
+        print('[+] Waiting for connections')
+        connection, client_address = sock.accept()
+        child_pid = os.fork()
+        if child_pid == 0:
+            # Child process
+            handle_connection(connection, client_address)
+        else :
+            # Parent process
+            active_children.append(child_pid)
+
+def main():
+    server_init()
+
+if __name__ == '__main__':
+    main()
